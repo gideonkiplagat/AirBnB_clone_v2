65505b81969c493d8630f0d305dcee83a1b25084
gideonkiplagat <gideonkiplagat00@gmail.com>
2024-04-04 19:54:49 +0300

    start server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,24 @@
-#!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+#!/bin/bash
 
-apt-get update
-apt-get install -y nginx
+# Install Nginx if not already installed
+sudo apt-get update
+sudo apt-get install -y nginx
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create necessary directories if they don't exist
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+# Create a fake HTML file for testing
+echo "<html><head></head><body>Test content for Nginx configuration</body></html>" | sudo tee /data/web_static/releases/test/index.html
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+# Create symbolic link
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
+# Give ownership to ubuntu user and group recursively
+sudo chown -R ubuntu:ubuntu /data/
 
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
+# Update Nginx configuration
+sudo sed -i '/server_name _;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+# Restart Nginx
+sudo service nginx restart
